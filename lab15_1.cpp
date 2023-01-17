b551dc3086c4f81216c454c1c73721799a8723ea
MindIssawara <kmind46@gmail.com>
2023-01-17 14:52:14 +0700

    lab15_1 completed

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,7 +3,22 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+    int i,j;
+    T value;
+	for(i=1;i<N;i++){
+		j=i-1;
+		value = d[i];
+		while(j>=0 && value>d[j]){
+			d[j+1]=d[j];
+			j--;
+		}
+		d[j+1]=value;
+		cout<<"Pass "<<i<<":";
+		for(j=0;j<N;j++){
+		    cout<< d[j] << " ";
+		}
+		cout<<"\n";
+	}
 }
 
 int main(){
